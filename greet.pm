b38261b348d0d70ca4b3cd960cd031bbd07db134
Doug Bell <madcityzen@gmail.com>
2013-06-13 13:04:12 -0500

    move GetOptionsFromArray into sub main() because it makes more sense

--- greet.pm
+++ greet.pm
@@ -7,22 +7,20 @@ use Getopt::Long qw( GetOptionsFromArray );
 use Pod::Usage;
 
 sub main {
-    my ( $opt, $argv ) = @_;
-    pod2usage(0) if $opt->{help};
-    pod2usage("ERROR: Must give someone to greet") if !$argv->[0];
-    $opt->{greeting} ||= "Hello";
-    say "$opt->{greeting}, $argv->[0]!";
-    return 0;
-}
-
-if ( !caller(0) ) {
+    my ( $argv ) = @_;
     my %opt;
-    GetOptionsFromArray( \@ARGV, \%opt,
+    GetOptionsFromArray( $argv, \%opt,
         'greeting:s',
         'help|h',
     );
-    exit main( \%opt, \@ARGV );
+    pod2usage(0) if $opt{help};
+    pod2usage("ERROR: Must give someone to greet") if !$argv->[0];
+    $opt{greeting} ||= "Hello";
+    say "$opt{greeting}, $argv->[0]!";
+    return 0;
 }
+
+exit main( \@ARGV ) if !caller(0);
 
 1;
 
--- greet.t
+++ greet.t
@@ -2,7 +2,7 @@ use Test::More;
 use Capture::Tiny qw( capture );
 use greet;
 my ( $stdout, $stderr, $exit ) = capture {
-    greet::main( {}, [ "Chicago.PM" ] );
+    greet::main( [ "Chicago.PM" ] );
 };
 is $stdout, "Hello, Chicago.PM!\n", 'correct greeting';
 is $exit, 0, 'exit code: success';
