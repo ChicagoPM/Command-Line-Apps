5bff7517e7fb8668ae71e42f14639aaf7ac33f3c
Doug Bell <madcityzen@gmail.com>
2013-06-13 12:49:38 -0500

    change for <STDIN> to while <STDIN>

--- find_emails.pl
+++ find_emails.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use feature qw( say );
-for my $line ( <STDIN> ) {
+while ( my $line = <STDIN> ) {
     my @emails = ( $line =~ /\b(\S+\@\S+)\b/g );
     say for @emails;
 }
